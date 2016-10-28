928e6d54d0017a280957153be6c8ea340490c586
fowee <847435887@qq.com>
2016-10-28 21:14:38 +0800

    20161028v001
    add game

---
+++ js/js005.js
@@ -0,0 +1,13 @@
+function Run() {
+    console.log('run...');
+
+    var $img = $('<img>');
+
+    $img.attr('src', './img/p01.jpg');
+
+    $img.attr('class', 'img-thumbnail');
+
+    $img.appendTo('#output');
+}
+
+$('#run').bind('click', Run);
