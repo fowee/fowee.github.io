6ddc4550041ec9754f142b455d81f3060158ced0
fowee <847435887@qq.com>
2016-11-14 13:52:30 +0800

    20161114.v004

--- js/d3.js
+++ js/d3.js
@@ -14,11 +14,11 @@ function Run() {
 case 0:{    r1.attr('x',50).attr('y',5).attr('width',203).attr('height',203).attr('style','fill:white').attr('stroke','black').attr('stroke-width',6);
     break;
 }
-case 1:{    r2.attr('x',110).attr('y',105).attr('width',80).attr('height',77).attr('style','fill:white').attr('stroke','black').attr('stroke-width',6)
+case 1:{    p1.attr('d','M 130 30 q -5 40 -60 70').attr('stroke','black').attr('stroke-width',5).attr('fill','none');
     break;}
-case 2:{    p1.attr('d','M 130 30 q -5 40 -60 70').attr('stroke','black').attr('stroke-width',5).attr('fill','none');
+case 2:{    p2.attr('d','M 170 30 q 5 40 60 70').attr('stroke','black').attr('stroke-width',5).attr('fill','none');
     break;}
-case 3:{    p2.attr('d','M 170 30 q 5 40 60 70').attr('stroke','black').attr('stroke-width',5).attr('fill','none');
+case 3:{    r2.attr('x',110).attr('y',105).attr('width',80).attr('height',77).attr('style','fill:white').attr('stroke','black').attr('stroke-width',6)
     break;}
 case 4:{    r2.attr('x',0).attr('y',0).attr('width',500).attr('height',500).attr('style','fill:white').attr('stroke','white').attr('stroke-width',0)
     break;}
