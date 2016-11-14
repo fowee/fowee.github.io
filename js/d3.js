6b0c9a18b07c7e7b9297653c39eb8da5be7f73bf
fowee <847435887@qq.com>
2016-11-14 13:50:47 +0800

    20161114.v003

--- js/d3.js
+++ js/d3.js
@@ -6,9 +6,10 @@ function Run() {
     var svgctrl = d3.select('svg');
     var r1 = svgctrl.append('rect');
     var r2 = svgctrl.append('rect');
+    var r3 = svgctrl.append('rect');   
     var p1 = svgctrl.append('path');
     var p2 = svgctrl.append('path');
-    switch(i%4)
+    switch(i%5)
 {
 case 0:{    r1.attr('x',50).attr('y',5).attr('width',203).attr('height',203).attr('style','fill:white').attr('stroke','black').attr('stroke-width',6);
     break;
@@ -18,6 +19,8 @@ case 1:{    r2.attr('x',110).attr('y',105).attr('width',80).attr('height',77).at
 case 2:{    p1.attr('d','M 130 30 q -5 40 -60 70').attr('stroke','black').attr('stroke-width',5).attr('fill','none');
     break;}
 case 3:{    p2.attr('d','M 170 30 q 5 40 60 70').attr('stroke','black').attr('stroke-width',5).attr('fill','none');
+    break;}
+case 4:{    r2.attr('x',0).attr('y',0).attr('width',500).attr('height',500).attr('style','fill:white').attr('stroke','white').attr('stroke-width',0)
     break;}
 }
 }
