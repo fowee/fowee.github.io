51057639456b4cc77f384b0b3071d4f9fc8e4d32
fowee <847435887@qq.com>
2017-03-20 16:48:16 +0800

    20170320v003
    modify

--- js/textxyz4.js
+++ js/textxyz4.js
@@ -1,9 +1,9 @@
 // HTML5的DeviceOrientation API 可实时获取屏幕的旋转方向。
 
-// alpha  设备绕z轴旋转的角度。 （指南针的应用只要拿到alpha就OK啦）
+// alpha  设备绕z轴旋转的角度。 （指南针的应用只要拿到alpha就OK）
 // beta   设备绕x轴旋转的角度
 // gamma  设备绕y轴旋转的角度
-// webkitCompassHeading: 与正北方向的角度差值。正北为0度，正东为90度，正南为180度，正西为270度。因为0度是正北，所以叫指北针，不是指南针。
+// webkitCompassHeading: 与正北方向的角度差值。正北为0度，正东为90度，正南为180度，正西为270度。
 // webkitCompassAccuracy：指北针的精确度，表示偏差为正负多少度。一般是10。
 
 //检查手机是否支持
